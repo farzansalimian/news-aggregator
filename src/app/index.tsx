925866e44f72904aaaed7c4e709314a16a6612de
farzan <farzan@bettermode.com>
2025-11-15 04:21:40 +0330

    feat: add redux persist

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { RoutePaths } from '@/constants/routes'
-import { store } from '@/store'
+import { persistor, store } from '@/store'
+import { PersistGate } from 'redux-persist/integration/react'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import { HomePage } from '@/pages/home'
@@ -10,15 +11,17 @@ import { Layout } from './layout'
 function App() {
   return (
     <Provider store={store}>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path={RoutePaths.HOME} element={<HomePage />} />
-            <Route path={RoutePaths.FEED} element={<FeedPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <Layout>
+            <Routes>
+              <Route path={RoutePaths.HOME} element={<HomePage />} />
+              <Route path={RoutePaths.FEED} element={<FeedPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </Layout>
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,16 +1,41 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import { ReducersName } from './constants'
 import { feedSettingReducer } from './feed-setting'
 import { newsApi } from '@/api/news'
 
-export const store = configureStore({
-  reducer: {
-    [ReducersName.FeedSetting]: feedSettingReducer,
-    [newsApi.reducerPath]: newsApi.reducer,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(newsApi.middleware),
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+
+const rootReducer = combineReducers({
+  [ReducersName.FeedSetting]: feedSettingReducer,
+  [newsApi.reducerPath]: newsApi.reducer,
 })
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: [ReducersName.FeedSetting],
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [
+          'persist/PERSIST',
+          'persist/REHYDRATE',
+          'persist/FLUSH',
+          'persist/PAUSE',
+          'persist/PURGE',
+          'persist/REGISTER',
+        ],
+      },
+    }).concat(newsApi.middleware),
+})
+export const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
